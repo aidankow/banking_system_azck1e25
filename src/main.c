88dfd2094ac007db3418901e4b1b95ed21eeee31
Aidan Kow <aidankow6@gmail.com>
2025-11-01 15:55:13 +0800

    implemented transaction log, fixed time format, improved UI

--- src/main.c
+++ src/main.c
@@ -22,12 +22,20 @@ bool validAction(char *action) {
     return valid;
 }
 
-int main() {
-    time_t currentTime;
+int logAction(char *action) {
+    FILE *transactionPtr;
+    transactionPtr = fopen("/Users/kuanhuakow/Library/CloudStorage/OneDrive-UniversityofSouthampton/banking_system_azck1e25/database/transaction.log", "a");
+    if (transactionPtr == NULL) {
+        perror("Error opening transaction.log");
+        return -1;
+    }
+    fprintf(transactionPtr, "%s\n", action);
+    fclose(transactionPtr);
+    return 1;
+}
+
+int getAction() {
     char action[11];
-    
-    time(&currentTime);
-    printf("\n\033[2;3mSession Start: %s\033[0m", ctime(&currentTime));
 
     printf("\n------------------------------------\n");
     printf("\033[1mChoose an Action by Entering a Number: \033[0m\n");
@@ -35,17 +43,42 @@ int main() {
     printf("[2] delete: Delete Bank Account\n");
     printf("[3] deposit: Deposit Money\n");
     printf("[4] withdrawl: Withdrawal Money\n");
-    printf("[5] remittance: Transfer Money to Another Account");
+    printf("[5] remittance: Transfer Money to Another Account\n");
+    printf("Enter 'EXIT' to exit the program.");
     printf("\n------------------------------------\n");
     printf("Selected Action: ");
     scanf("%10s", action);
     clearInputBuffer();
-    
+
+    if (strcmp(action, "EXIT") == 0) {
+        printf("\n\033[2;3mSession Ended.\033[0m\n");
+        return 0;
+    }
+
     while (!validAction(action)) {
         printf("\033[31m**ERROR: INVALID ACTION**\033[0m\n");
         printf("Please Enter a Valid Action: ");
         scanf("%10s", action);
         clearInputBuffer();
+    }
+    
+    printf("\n*\n*\n*\n*\n*\n*\n*\n");
+    return logAction(action);
+}
+
+int main() {
+    char formattedTime[64];
+    int action = -1;
+
+    time_t now = time(NULL);
+    struct tm *time = localtime(&now);
+    char buffer[100];
+    
+    strftime(buffer, sizeof(buffer), "%B %d, %A %H:%M:%S", time);
+    printf("\n\033[2;3mSession Start: %s\033[0m\n", buffer);
+
+    while (action != 0) {
+        action = getAction();
     }
 
     return 0;
